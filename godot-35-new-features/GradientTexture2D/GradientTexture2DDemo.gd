ee2c66897674b22da57c96488edbc1410fe2c931
Xananax <me@xananax.com>
2022-06-27 15:38:32 +0200

    gradientTexture2D: add particles and unify resource

--- godot-35-new-features/GradientTexture2D/GradientTexture2DDemo.gd
+++ godot-35-new-features/GradientTexture2D/GradientTexture2DDemo.gd
@@ -1,12 +1,4 @@
 extends Control
 
-onready var light_2d := get_node("%Light2D") as Light2D
-onready var csg_mesh := get_node("%GSGMesh") as CSGMesh
-
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
+	pass
