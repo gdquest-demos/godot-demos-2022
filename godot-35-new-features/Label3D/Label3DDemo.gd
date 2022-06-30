354c94faa16b04266d218f60a459bd5348e4ebef
Nathan Lovato <12694995+NathanLovato@users.noreply.github.com>
2022-06-30 16:16:09 +0200

    clean up and fix label 3d scene

--- godot-35-new-features/Label3D/Label3DDemo.gd
+++ godot-35-new-features/Label3D/Label3DDemo.gd
@@ -2,27 +2,33 @@ extends Spatial
 
 onready var billboard_check_box: CheckBox = $CanvasLayer/PanelContainer/MarginContainer/VBoxContainer/BillboardCheckBox
 onready var depth_test_check_box: CheckBox = $CanvasLayer/PanelContainer/MarginContainer/VBoxContainer/DepthTestCheckBox
-onready var label_3d: Label3D = $NavigationEnemy3D/Label3D
+onready var enemy_label: Label3D = $NavigationEnemy3D/Label3D
+onready var player_label: Label3D = $Player3D/Label3D
+
+onready var labels := [enemy_label, player_label]
+
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
-	billboard_check_box.pressed = label_3d.billboard
+	billboard_check_box.pressed = enemy_label.billboard
 	billboard_check_box.connect("toggled", self, "_on_BillboardCheckbox_toggled")
 	
-	depth_test_check_box.pressed = label_3d.no_depth_test
+	depth_test_check_box.pressed = enemy_label.no_depth_test
 	depth_test_check_box.connect("toggled", self, "_on_DepthTestCheckbox_toggled")
 
 
 func _on_BillboardCheckbox_toggled(toggled: bool) -> void:
-	label_3d.billboard = toggled
+	for label in labels:
+		label.billboard = toggled
 	
 
 func _on_DepthTestCheckbox_toggled(toggled: bool) -> void:
-	label_3d.no_depth_test = toggled
-	if toggled:
-		label_3d.render_priority = 2
-		label_3d.outline_render_priority = 1
-	else:
-		label_3d.render_priority = 0
-		label_3d.outline_render_priority = -1
+	for label in labels:
+		label.no_depth_test = toggled
+		if toggled:
+			label.render_priority = 2
+			label.outline_render_priority = 1
+		else:
+			label.render_priority = 0
+			label.outline_render_priority = -1
