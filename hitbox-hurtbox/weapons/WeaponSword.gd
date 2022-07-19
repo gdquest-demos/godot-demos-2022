1932b329a1b1d239d3cd2ec8dd08dccbf3098cef
Nathan Lovato <12694995+NathanLovato@users.noreply.github.com>
2022-07-19 15:48:56 +0200

    update hurtbox demo, add readme

--- hitbox-hurtbox/weapons/WeaponSword.gd
+++ hitbox-hurtbox/weapons/WeaponSword.gd
@@ -1,6 +1,5 @@
 extends Node2D
 
-var follow_cursor := true
 
 onready var _animation_player := $AnimationPlayer
 
@@ -8,15 +7,6 @@ onready var _animation_player := $AnimationPlayer
 func use() -> void:
 	_animation_player.play("slash")
 
-
-func _physics_process(_delta: float) -> void:
-	if follow_cursor:
-		look_at(get_global_mouse_position())
-
-
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_released("ui_cancel"):
-		follow_cursor = !follow_cursor
-	
 	if event.is_action_pressed("attack"):
 		use()
