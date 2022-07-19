b507abf7bf6cb06068fa4629c2181583274e035a
johnnygossdev <me@johnnygoss.dev>
2022-07-19 11:06:15 +0100

    remove unused weapon scene

--- hitbox-hurtbox/weapons/Weapon.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-export var damage := 10
-
-onready var animation_player := $AnimationPlayer
-
-
-func use() -> void:
-	pass
--- juicy-attack/weapons/Weapon.gd
+++
@@ -1,5 +0,0 @@
-extends Node2D
-
-
-func use() -> void:
-	pass
