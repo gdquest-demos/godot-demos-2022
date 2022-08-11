14a2c0b80c321b1c00db022683bb2454a2f975f0
Nathan Lovato <12694995+NathanLovato@users.noreply.github.com>
2022-08-11 16:28:25 +0200

    chore: tweak collision layers and masks

--- hitbox-hurtbox/nodes/HitBox.gd
+++ hitbox-hurtbox/nodes/HitBox.gd
@@ -6,6 +6,6 @@ export var damage := 10
 
 
 func _init() -> void:
-	collision_layer = 0
+	collision_mask = 0
 	# This turns off collision mask bit 1 and turns on bit 2. It's the physics layer we reserve to hurtboxes in this demo.
-	collision_mask = 2
+	collision_layer = 2
--- hitbox-hurtbox/nodes/HurtBox.gd
+++ hitbox-hurtbox/nodes/HurtBox.gd
@@ -7,7 +7,7 @@ func _init() -> void:
 	# The hurtbox should detect hits but not deal them. This variable does that.
 	monitorable = false
 	# This turns off collision layer bit 1 and turns on bit 2. It's the physics layer we reserve to hurtboxes in this demo.
-	collision_layer = 2
+	collision_mask = 2
 
 
 func _ready() -> void:
