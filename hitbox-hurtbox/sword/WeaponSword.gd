a99cdd87409c250f32db9c0adadbbc1a695ab6ac
Nathan Lovato <12694995+NathanLovato@users.noreply.github.com>
2022-08-11 16:06:23 +0200

    improvement: simplify hitbox hurtbox demo, add practice folder

--- hitbox-hurtbox/sword/WeaponSword.gd
+++ hitbox-hurtbox/sword/WeaponSword.gd
@@ -1,6 +1,22 @@
 extends Node2D
 
+const DRAG_FACTOR := 15.0
+const RUN_SPEED := 800.0
+
+var _velocity := Vector2.ZERO
+
 onready var animation_player := $AnimationPlayer
+
+
+func _physics_process(delta: float) -> void:
+	var input_direction := Vector2.ZERO
+	input_direction = Vector2(
+				Input.get_axis("move_left", "move_right"), Input.get_axis("move_up", "move_down")
+			)
+	var desired_velocity := input_direction * RUN_SPEED
+	var steering = desired_velocity - _velocity
+	_velocity += steering * DRAG_FACTOR * delta
+	position += _velocity * delta
 
 
 func _unhandled_input(event: InputEvent) -> void:
