0af5da66788ec9cade6a9658978c9c9fb9edf3e1
Moises Pelaez <moises.jpelaez@gmail.com>
2022-08-30 15:02:31 -0400

    Player property renamed.

--- combo-demo/player/Player.gd
+++ combo-demo/player/Player.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
 var _accept_next_attack := true
-var _previous_light_animation := "light_combo_3"
+var _previous_light_combo := "light_combo_3"
 
 onready var _animation_player := $AnimationPlayer
 
@@ -9,7 +9,7 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("attack") and _accept_next_attack:
 		_accept_next_attack = false
 		
-		match _previous_light_animation:
+		match _previous_light_combo:
 			"light_combo_1":
 				_animation_player.play("light_combo_2")
 			"light_combo_2":
@@ -17,7 +17,7 @@ func _unhandled_input(event: InputEvent) -> void:
 			"light_combo_3":
 				_animation_player.play("light_combo_1")
 		
-		_previous_light_animation = _animation_player.current_animation
+		_previous_light_combo = _animation_player.current_animation
 
 
 func _ready() -> void:
@@ -30,4 +30,4 @@ func set_accept_next_attack() -> void:
 
 func _on_AnimationPlayer_finished(animation_name: String) -> void:
 	if "light_combo" in animation_name:
-		_previous_light_animation = "light_combo_3"
+		_previous_light_combo = "light_combo_3"
