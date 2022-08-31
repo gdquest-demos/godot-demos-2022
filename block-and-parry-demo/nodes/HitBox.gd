fddd4cdb1de4fc145e07aa3cec5b02896fb68e44
Moises Pelaez <moises.jpelaez@gmail.com>
2022-08-31 11:36:08 -0400

    Replace HitBox and HurtBox folder content.

--- block-and-parry-demo/nodes/HitBox.gd
+++ block-and-parry-demo/nodes/HitBox.gd
@@ -2,13 +2,10 @@
 class_name HitBox, "HitBox.svg"
 extends Area2D
 
-export var damage := 5
+export var damage := 10
 
 
 func _init() -> void:
-	# This turns off collision layer bit 1 and turns on bit 2. It's the physics layer we reserve to hurtboxes in this demo.
+	collision_mask = 0
+	# This turns off collision mask bit 1 and turns on bit 2. It's the physics layer we reserve to hurtboxes in this demo.
 	collision_layer = 2
-
-
-func get_damage() -> int:
-	return damage + randi() % 7 - 3
--- block-and-parry-demo/nodes/HurtBox.gd
+++ block-and-parry-demo/nodes/HurtBox.gd
@@ -6,7 +6,7 @@ extends Area2D
 func _init() -> void:
 	# The hurtbox should detect hits but not deal them. This variable does that.
 	monitorable = false
-	# This turns off collision mask bit 1 and turns on bit 2. It's the physics layer we reserve to hurtboxes in this demo.
+	# This turns off collision layer bit 1 and turns on bit 2. It's the physics layer we reserve to hurtboxes in this demo.
 	collision_mask = 2
 
 
@@ -16,6 +16,4 @@ func _ready() -> void:
 
 func _on_area_entered(hitbox: HitBox) -> void:
 	if owner.has_method("take_damage"):
-		owner.take_damage(hitbox.get_damage())
-	if owner.has_method("knock_back"):
-		owner.knock_back(hitbox.global_position)
+		owner.take_damage(hitbox.damage)
