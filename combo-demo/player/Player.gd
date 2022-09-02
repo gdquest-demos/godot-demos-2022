8b00dae203717238d6ebcff7035eec761898c699
Moises Pelaez <moises.jpelaez@gmail.com>
2022-09-02 10:22:07 -0400

    Refactor attempt for handling combos.

--- combo-demo/player/Player.gd
+++ combo-demo/player/Player.gd
@@ -2,9 +2,16 @@ extends KinematicBody2D
 
 enum States { MOVE, LIGHT_ATTACK, HEAVY_ATTACK }
 
+enum { LIGHT_ATTACK, HEAVY_ATTACK }
+
 const DRAG_FACTOR := 15.0
 const SPEED_MOVE := 400.0
 const SPEED_ATTACK := 1000.0
+
+const COMBOS = {
+	LIGHT_ATTACK: ["light_combo_1", "light_combo_2", "light_combo_3"],
+	HEAVY_ATTACK: ["heavy_combo_1", "heavy_combo_2"]
+}
 
 var accept_next_attack := true setget set_accept_next_attack
 
@@ -12,8 +19,9 @@ var _state = States.MOVE
 var _direction := Vector2.ZERO
 var _velocity := Vector2.ZERO
 
-var _previous_light_combo := "light_combo_3"
-var _previous_heavy_combo := "heavy_combo_2"
+var _previous_attack := LIGHT_ATTACK
+var _current_attack := LIGHT_ATTACK
+var _current_attack_index := -1
 
 onready var _skin := $Skin
 onready var _animation_player := $AnimationPlayer
@@ -21,41 +29,15 @@ onready var _animation_player := $AnimationPlayer
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("light_attack") and accept_next_attack:
-		_previous_heavy_combo = "heavy_combo_2"
-		
 		_state = States.LIGHT_ATTACK
-		accept_next_attack = false
-		
-		_animation_player.stop()
-		_animation_player.play("idle")
-		
-		match _previous_light_combo:
-			"light_combo_1":
-				_animation_player.play("light_combo_2")
-			"light_combo_2":
-				_animation_player.play("light_combo_3")
-			"light_combo_3":
-				_animation_player.play("light_combo_1")
-		
-		_previous_light_combo = _animation_player.current_animation
+		_current_attack = LIGHT_ATTACK
+		attack()
 		return
 	
 	if event.is_action_pressed("heavy_attack") and accept_next_attack:
-		_previous_light_combo = "light_combo_3"
-		
 		_state = States.HEAVY_ATTACK
-		accept_next_attack = false
-		
-		_animation_player.stop()
-		_animation_player.play("idle")
-		
-		match _previous_heavy_combo:
-			"heavy_combo_1":
-				_animation_player.play("heavy_combo_2")
-			"heavy_combo_2":
-				_animation_player.play("heavy_combo_1")
-		
-		_previous_heavy_combo = _animation_player.current_animation
+		_current_attack = HEAVY_ATTACK
+		attack()
 		return
 
 
@@ -79,14 +61,26 @@ func _physics_process(delta: float) -> void:
 	move_and_collide(_velocity * delta)
 
 
+func attack() -> void:
+	accept_next_attack = false
+	
+	_animation_player.stop()
+	_animation_player.play("idle")
+	
+	if _previous_attack != _current_attack:
+		_current_attack_index = -1
+	
+	_current_attack_index = (_current_attack_index + 1) % COMBOS[_current_attack].size() 
+	_animation_player.play(COMBOS[_current_attack][_current_attack_index])
+	
+	_previous_attack = _current_attack
+
+
 func set_accept_next_attack(value := true) -> void:
 	accept_next_attack = value
   
 
 func _on_AnimationPlayer_finished(animation_name: String) -> void:
-	if "light_combo" in animation_name:
-		_previous_light_combo = "light_combo_3"
-		_state = States.MOVE
-	elif "heavy_combo" in animation_name:
-		_previous_light_combo = "heavy_combo_2"
+	if "combo" in animation_name:
+		_current_attack_index = -1
 		_state = States.MOVE
