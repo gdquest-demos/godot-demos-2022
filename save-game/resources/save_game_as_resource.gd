0a7d6f2a7847e1834f3e7856c2c5d655ce68019b
Nathan Lovato <12694995+NathanLovato@users.noreply.github.com>
2025-09-26 13:51:59 +0200

    remove unnecessary workaround Godot 3 caching bug

--- save-game/resources/save_game_as_resource.gd
+++ save-game/resources/save_game_as_resource.gd
@@ -1,6 +1,6 @@
 # Save and load the game using the text or binary resource format.
 #
-# /!\ This approach is unsafe if players download completed save games from the 
+# /!\ This approach is unsafe if players download completed save games from the
 # web. Please read the README and watch the corresponding video about security
 # issues.
 class_name SaveGameAsResource extends Resource
@@ -26,7 +26,7 @@ const SAVE_GAME_BASE_PATH := "user://save"
 
 
 # The next three functions are just to keep the save API inside of the SaveGameAsResource resource.
-# Note that this has safety issues if players download savegame files from the 
+# Note that this has safety issues if players download savegame files from the
 # web. Please see the README and check out the dedicated video.
 # For a safe alternative, see the function write/load_as_json() below.
 func write_savegame() -> void:
@@ -39,51 +39,7 @@ static func save_exists() -> bool:
 
 static func load_savegame() -> Resource:
 	var save_path := get_save_path()
-	if ResourceLoader.has_cached(save_path):
-		# Once the resource caching bug is fixed, you will only need this line of code to load the save game.
-		return ResourceLoader.load(save_path, "", ResourceLoader.CACHE_MODE_IGNORE)
-
-	# /!\ Workaround for bug https://github.com/godotengine/godot/issues/59686
-	# Without that, sub-resources will not reload from the saved data.
-	# We copy the SaveGameAsResource resource's data to a temporary file, load that file
-	# as a resource, and make it take over the save game.
-
-	# We first load the save game resource's content as a byte array and store it.
-	var file := FileAccess.open(save_path, FileAccess.READ)
-	if not file:
-		printerr("Couldn't read file " + save_path)
-		return null
-
-	var data := file.get_buffer(file.get_length())
-	file.close()
-
-	# Then, we generate a random file path that's not in Godot's cache.
-	var tmp_file_path := make_random_path()
-	while ResourceLoader.has_cached(tmp_file_path):
-		tmp_file_path = make_random_path()
-
-	# We write a copy of the save game to that temporary file path.
-	file = FileAccess.open(tmp_file_path, FileAccess.WRITE)
-	if not file:
-		printerr("Couldn't write file " + tmp_file_path)
-		return null
-
-	file.store_buffer(data)
-	file.close()
-
-	# We load the temporary file as a resource.
-	var save = ResourceLoader.load(tmp_file_path, "", ResourceLoader.CACHE_MODE_IGNORE)
-	# And make it take over the save path for the next time the player
-	# saves.
-	save.take_over_path(save_path)
-
-	# We delete the temporary file.
-	DirAccess.remove_absolute(tmp_file_path)
-	return save
-
-
-static func make_random_path() -> String:
-	return "user://temp_file_" + str(randi()) + ".tres"
+	return ResourceLoader.load(save_path, "", ResourceLoader.CACHE_MODE_IGNORE)
 
 
 # This function allows us to save and load a text resource in debug builds and a
