7597bc8416e2f84ea5669385f43382ac9b3c6a13
Răzvan C. Rădulescu <razcore.rad@gmail.com>
2025-10-06 17:31:05 +0300

    chore: `hitbox-hurtbox` properties & code to reflect the old vid & new code

--- hitbox-hurtbox/nodes/hit_area_2d.gd
+++ hitbox-hurtbox/nodes/hit_area_2d.gd
@@ -6,6 +6,7 @@ class_name HitArea2D extends Area2D
 
 
 func _ready() -> void:
-	collision_mask = 0
-	# This turns off collision mask bit 1 and turns on bit 2. It's the physics layer we reserve to hurtboxes in this demo.
+	# This turns off collision mask bit 1 and turns on bit 2.
+	# It's the physics layer we reserve to hurtboxes in this demo.
 	collision_layer = 2
+	collision_mask = 0
--- hitbox-hurtbox/nodes/hurt_area_2d.gd
+++ hitbox-hurtbox/nodes/hurt_area_2d.gd
@@ -6,7 +6,8 @@ func _ready() -> void:
 	area_entered.connect(_on_area_entered)
 	# The hurtbox should detect hits but not deal them. This variable does that.
 	monitorable = false
-	# This turns off collision layer bit 1 and turns on bit 2. It's the physics layer we reserve to hurtboxes in this demo.
+	# This turns off collision layer bit 1 and turns on bit 2.
+	# It's the physics layer we reserve to hurtboxes in this demo.
 	collision_mask = 2
 
 
